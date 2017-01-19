76d4ce256d36d84f3d5d93538772296ddc654976
lasthulk <danchithancongab@gmail.com>
2017-01-19 16:58:55 +0700

    update

--- src/golang/chapter2/struct2.go
+++ src/golang/chapter2/struct2.go
@@ -1,6 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
+	p := Person{
+		name: "toan",
+		address: Address{
+			city:     "SG",
+			district: "1",
+			ward:     "Da Kao",
+		},
+	}
+
+	p.Talk()
+	p.Location()
+
+	c := Citizen{
+		country: "VN",
+		person:  p,
+	}
+	c.Nationality()
+}
+
+type Citizen struct {
+	country string
+	person  Person
+}
+
+func (c Citizen) Nationality() {
+	fmt.Println(c.person.name, "is a citizen of", c.country)
 }
 
 type Address struct {
@@ -12,4 +40,12 @@ type Address struct {
 type Person struct {
 	name    string
 	address Address
+}
+
+func (p Person) Talk() {
+	fmt.Println("Hi, my name is", p.name)
+}
+
+func (p Person) Location() {
+	fmt.Println("I’m at", p.address.city, p.address.district, p.address.ward)
 }
